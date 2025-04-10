e3b22886133f965603796319f705a2f6fd4073e9
William Line <william.line@proton.me>
2025-04-11 00:37:37 +0100

    made file upload code

--- application.py
+++ application.py
@@ -1,16 +1,25 @@
 from flask import Flask, redirect, url_for, request, render_template, jsonify, flash, send_file, Blueprint
-
+import gpxpy
 
 application = Flask(__name__)
 application.secret_key = "super secret key" #DO NOT LEAVE THIS LIKE THIS
 
+application.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+application.config['UPLOAD_EXTENSIONS'] = ['.gpx']
+
 @application.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',elevation="Submit file and coordinates to find out")
 
+@application.route('/',methods=['POST'])
+def file_upload():
+    uploaded_file = request.files['file']
+
+    elevation=0
+    return render_template('index.html',elevation=elevation)
 
 if __name__ == '__main__':
-   website_url='involuntaryCTF:5000'
+   website_url='abitofmapreading.involuntaryCTF:5000'
    application.config['SERVER_NAME']=website_url
    application.run(debug=True)
 
